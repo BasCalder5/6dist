56ec52062234052c24b502194270c73300d70f9e
BasCalder5 <62661069+BasCalder5@users.noreply.github.com>
2020-03-29 13:44:26 +0200

    Added server

--- communications/src/Main.java
+++ communications/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Communicationstest\n");
+        System.out.println("--------------------Communicationstest Start-------------------\n");
     }
 }
---
+++ communications/src/Server.java
@@ -0,0 +1,35 @@
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private static ServerSocket piServer;
+    private static int port = 1234;
+
+    public static void main(String args[]) throws IOException, ClassNotFoundException {
+        piServer = new ServerSocket(port); // server socket aanmaken
+
+        while(true){
+            System.out.println("Waiting...");
+            Socket socket = piServer.accept();
+
+            ObjectInputStream inputs = new ObjectInputStream(socket.getInputStream()); //lees input stream van socket
+            String message = (String) inputs.readObject();
+            System.out.println("Recieved message: " + message);
+
+            ObjectOutputStream outputs = new ObjectOutputStream(socket.getOutputStream()); //maak output stream voor socket
+            outputs.writeObject("Hello world! " + message);
+
+            inputs.close();
+            outputs.close();
+            socket.close();
+
+            if(message.equalsIgnoreCase("corona")) break;
+        }
+        System.out.println("Shut down!");
+        piServer.close();
+    }
+}
